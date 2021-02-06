f98ee18e2ce6fe33b63632d179c126ad4428eacd
Ruslan <76034292+shadymady@users.noreply.github.com>
2021-02-07 00:00:42 +0300

    taskHibernate

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,14 +1,52 @@
 package jm.task.core.jdbc.util;
+
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
+
+    private static SessionFactory sessionFactory = null;
+
+    public SessionFactory getSessionFactory(){
+        try{
+            Configuration configuration = new Configuration();
+            Properties settings = new Properties();
+            settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+            settings.put(Environment.URL, "jdbc:mysql://localhost:3306/testbase?serverTimezone=UTC");
+            settings.put(Environment.USER, "root");
+            settings.put(Environment.PASS, "root");
+            settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+
+            configuration.setProperties(settings);
+            configuration.addAnnotatedClass(User.class);
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+        return sessionFactory;
+    }
+
     Connection connection;
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     static final String DB_URL = "jdbc:mysql://localhost:3306/testbase?useSSL=false&serverTimezone=UTC";
     static final String LOGIN = "root";
     static final String PASSWORD = "root";
+
     public Connection getConnection(){
         try{
             Class.forName(JDBC_DRIVER);
@@ -19,8 +57,3 @@ public class Util {
         return connection;
     }
 }
-
-
-
-
-
