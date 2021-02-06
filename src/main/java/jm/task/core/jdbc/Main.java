da6decdf80a6ee8a94ef79da4e6149e6cd54ff71
Ruslan <76034292+shadymady@users.noreply.github.com>
2021-02-07 00:01:51 +0300

    taskHibernate

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
@@ -10,7 +11,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws SQLException {
         List<User> user;
-        UserDaoJDBCImpl table = new UserDaoJDBCImpl();
+        UserDaoHibernateImpl table = new UserDaoHibernateImpl();
         table.createUsersTable();
         table.saveUser("Zara", "Ali", (byte) 18);
         table.saveUser("Faruk", "Nari", (byte) 24);
